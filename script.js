111ec4e7e5f313bbbe5330a5bf36bd10fc37a5f1
poojayadav232 <139120322+poojayadav232@users.noreply.github.com>
2023-07-15 10:11:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Engineer", "Web Developer"],
+        strings: ["Software Engineer", "Web Developer "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
