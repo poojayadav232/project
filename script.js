ef1eecb5e206c20573bc65026df8165b5ae8164c
poojayadav232 <139120322+poojayadav232@users.noreply.github.com>
2023-07-15 10:10:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Azure Architect", "Cloud Enginner"],
+        strings: ["Software Engineer", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Cloud Expert, J2EE Expert"],
+        strings: ["Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
